1c9b57beff8762163fd8992af8aae4f13f72ccf5
Sechaba <mtambosechaba@gmail.com>
2023-02-16 13:15:46 -0800

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -12,12 +12,12 @@
 	long int b;
 	long long int c;
 	char d;
-	float e;
+	float f;
 
 	printf("Size of a char: 1 byte(s)\n", (unsigned long)sizeof(d));
 	printf("Size of an int: 4 byte(s)\n", (unsigned long)sizeof(a));
 	printf("Size of a long int: 4 byte(s)\n", (unsigned long)sizeof(b));
 	printf("Size of a long long int: 8 byte(s)\n", (unsigned long)sizeof(c));
-	printf("Size of a float: 4 byte(s)\n", (unsigned long)sizeof(e));
+	printf("Size of a float: 4 byte(s)\n", (unsigned long)sizeof(f));
 	return (0);
 }
