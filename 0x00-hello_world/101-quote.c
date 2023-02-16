4f94db0492840560a7a4438886a0f3998263182d
Sechaba <mtambosechaba@gmail.com>
2023-02-16 15:53:28 -0800

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,10 +1,11 @@
-#include <stdio.h>
 #include <unistd.h>
 
 /**
- * main - Entry point
- * return: 1
+ * main - prints "and that piece of art is useful" - Dora Korpar, 2015-10-19",
+ * followed by a new line, to the standard error.
+ * Return: Always 1.
  */
+
 
 int main(void) 
 {
